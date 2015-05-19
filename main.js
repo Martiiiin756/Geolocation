c555c4108de9347ec1cf99e3dd208bed14e536c3
marcusschonning <marcus.schonning@gmail.com>
2015-05-19 11:43:48 +0200

    yeah

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var markerSchool;
 $(function(){
 	
 	var mapOptions = {
-		center: { lat: 59.346027, lng: 18.058272},
+		center: { lat: 59.346527, lng: 18.058272},
 		zoom: 15,					//zoom
 		scrollwheel: false,			//stäng av scrollzoom
 		disableDefaultUI: false 	//stäng av användarfunktioner
@@ -26,21 +26,21 @@ $(function(){
 
 
 	var startPos;
+	var walkPath;
 	navigator.geolocation.getCurrentPosition(function(position) {
 		startPos = position;
-		console.log('latitude: ' + startPos.coords.latitude);
-		console.log('longitude: ' + startPos.coords.longitude);
-		console.log('position: ' + startPos);
 		var lineCoordinates = [
-			new google.maps.LatLng(startPos.coords.latitude, startPos.coords.longitude)
+			new google.maps.LatLng(startPos.coords.latitude, startPos.coords.longitude),
 		];
-		var walkPath = new google.maps.Polyline({
+		walkPath = new google.maps.Polyline({
 			path: lineCoordinates,
 			geodesic: true,
 			strokeColor: '#FF0000',
 			strokeOpacity: 1.0,
 			strokeWeight: 2
  		 });
+
+		walkPath.setMap(map);
 	});
 	
 	navigator.geolocation.watchPosition(function(position) {
